1ef1a7a69a5152c7273ecaea8074033df60ea3c3
Peter Hanson <peter@upandrunningsoftware.com>
2016-07-15 14:28:00 -0500

    refactored game.php to move scoring bonus and expressions into there own methods

--- kata/robert_martin_bowling_game_kata/iteration1/game.php
+++ kata/robert_martin_bowling_game_kata/iteration1/game.php
@@ -18,19 +18,31 @@ class Game {
 
             if ($this->isStrike($frameIndex)) {
 
-                $total = $total + 10 + $this->rolls[$frameIndex + 1] + $this->rolls[$frameIndex + 2];
+                $total = $total + 10 + $this->strikeBonus($frameIndex);
 
             } else if ($this->isSpare($frameIndex)) {
-               $total = $total + 10 + $this->rolls[$frameIndex + 2];
+               $total = $total + 10 + $this->spareBonus($frameIndex);
                 $frameIndex++;
             } else {
-                $total = $total + $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+                $total = $total + $this->sumOfBallsInFrame($frameIndex);
                 $frameIndex++;
             }
 
         }
 
         return $total;
+    }
+
+    protected function strikeBonus($frameIndex) {
+        return $this->rolls[$frameIndex + 1] + $this->rolls[$frameIndex + 2];
+    }
+
+    protected function spareBonus($frameIndex) {
+       return  $this->rolls[$frameIndex + 2];
+    }
+
+    protected function sumOfBallsInFrame($frameIndex) {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
     }
 
     protected function isSpare($frameIndex) {
